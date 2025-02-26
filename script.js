26745a084d9bdf9b0ede284d69e74f11dfa04525
Jhennifer Kymberlly <jhenniferkymberllysilva@gmail.com>
2025-02-26 20:19:45 -0300

    Código Refinado

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ const lamp = document.getElementById("lamp");
 const container = document.getElementById("container");
 const texto = document.getElementById("texto");
 
+function moverLampada(x, y) {
+    lamp.style.transform = `translate(${x}px, ${y}px)`;
+}
+
 function ligando(){
     lamp.src = './acesa.png';
     container.style.backgroundColor = "#fff"
     texto.innerText = "Apague a luz";
+        texto.style.color = "#000"
+    moverLampada(0, 0)
 }
 
 function apagando(){
-    lamp.src = './apagada.png';
+    lamp.src = './lampada.png';
     container.style.backgroundColor = "#17131d"
     texto.innerText = "Ligue a luz";
+    texto.style.color = "#fff"
+    moverLampada(0, 30);
 }
 
 ligada.addEventListener('click', ligando);
